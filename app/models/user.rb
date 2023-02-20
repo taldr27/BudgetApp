048b67d0f14e622657e94849105cea3a6e760932
lana <diegogarciaq98@gmail.com>
2023-02-20 11:24:49 -0500

    Add devise to the project

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :confirmable
   has_many :categories
   has_many :entities
 
   validates :name, presence: true
-  # validates :email, presence: true
+  validates :email, presence: true
 end
